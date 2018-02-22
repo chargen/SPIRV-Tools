0b015980f9ee797a67ccc47f941199d8f5535eaa
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2018-02-21 20:29:23 -0800

    Add storage class and builtin type checks to interface cleanup
    Also streamline control flow a bit.

--- source/opt/interface_cleanup_pass.cpp
+++ source/opt/interface_cleanup_pass.cpp
@@ -20,7 +20,8 @@
 namespace spvtools {
 namespace opt {
 namespace {
-const uint32_t kInterfaceOperandIdx = 3;
+const uint32_t kTypePointerStorageClassInIdx = 0;
+const uint32_t kEntryPointVariableIdx = 3;
 
 bool HasExternalLinkage(analysis::DecorationManager* decoration_mgr,
                         uint32_t result_id) {
@@ -48,7 +49,35 @@ bool IsUsedExceptInEntryPoint(analysis::DefUseManager* def_use_mgr,
   });
   return used;
 }
+
+SpvStorageClass GetStorageClass(analysis::DefUseManager* def_use_mgr,
+                                uint32_t result_id) {
+  ir::Instruction* def = def_use_mgr->GetDef(result_id);
+  assert(def != nullptr);
+  assert(def->opcode() == SpvOpVariable);
+
+  ir::Instruction* type = def_use_mgr->GetDef(def->type_id());
+  assert(type);
+  assert(type->opcode() == SpvOpTypePointer);
+
+  return SpvStorageClass(
+      type->GetSingleWordInOperand(kTypePointerStorageClassInIdx));
 }
+
+SpvBuiltIn GetBuiltInDecoration(analysis::DecorationManager* decoration_mgr,
+                                uint32_t result_id) {
+  SpvBuiltIn result = SpvBuiltInMax;
+  decoration_mgr->ForEachDecoration(
+      result_id, SpvDecorationBuiltIn,
+      [&result](const ir::Instruction& linkage_instruction) {
+        uint32_t last_operand = linkage_instruction.NumOperands() - 1;
+        result =
+            SpvBuiltIn(linkage_instruction.GetSingleWordOperand(last_operand));
+      });
+  return result;
+}
+
+}  // namespace
 
 Pass::Status InterfaceCleanupPass::Process(ir::IRContext* c) {
   InitializeProcessing(c);
@@ -58,19 +87,41 @@ Pass::Status InterfaceCleanupPass::Process(ir::IRContext* c) {
   for (auto& entrypoint : context()->module()->entry_points()) {
     assert(entrypoint.opcode() == SpvOpEntryPoint);
 
-    for (uint32_t op = kInterfaceOperandIdx; op < entrypoint.NumOperands();) {
+    for (uint32_t op = kEntryPointVariableIdx; op < entrypoint.NumOperands();) {
       uint32_t var = entrypoint.GetSingleWordInOperand(op);
 
-      if (HasExternalLinkage(get_decoration_mgr(), var) ||
-          IsUsedExceptInEntryPoint(get_def_use_mgr(), var)) {
+      if (HasExternalLinkage(get_decoration_mgr(), var)) {
         ++op;
-      } else {
-        modified = true;
-
-        entrypoint.RemoveOperand(op);
-
-        context()->KillDef(var);
+        continue;
       }
+
+      if (IsUsedExceptInEntryPoint(get_def_use_mgr(), var)) {
+        ++op;
+        continue;
+      }
+
+      auto storage_class = GetStorageClass(get_def_use_mgr(), var);
+      auto builtin = GetBuiltInDecoration(get_decoration_mgr(), var);
+
+      // output variables have undefined values if shader doesn't write them
+      // removing them might affect interface compatibility if the next stage
+      // reads them (this produces undefined values but otherwise is valid?)
+      if (storage_class != SpvStorageClassInput) {
+        ++op;
+        continue;
+      }
+
+      // presence of SampleId builtin affects semantics (forces sample shading)
+      if (builtin == SpvBuiltInSampleId) {
+        ++op;
+        continue;
+      }
+
+      modified = true;
+
+      entrypoint.RemoveOperand(op);
+
+      context()->KillDef(var);
     }
   }
 
